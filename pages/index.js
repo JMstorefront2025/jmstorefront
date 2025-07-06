6616177e210f1478dc1fa7d6ce8187451ea137e1
JH Home Mac <jhMac@Mac-Studio.local>
2025-07-05 18:09:58 -0700

    Remove test heading

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,5 @@
 import JMStorefront from "../src/JMStorefront.jsx";
 
 export default function Home() {
-  return (
-    <>
-      <h1>🚀 Live from JM Store!</h1>
-      <JMStorefront />
-    </>
-  );
+  return <JMStorefront />;
 }
